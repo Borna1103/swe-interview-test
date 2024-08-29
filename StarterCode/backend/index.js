8454def7dd4f2e1cc3117c86d983d915b178d17b
Borna <Borna1103@msn.com>
2024-08-29 01:10:02 -0700

    Status Update

--- StarterCode/backend/index.js
+++ StarterCode/backend/index.js
@@ -48,7 +48,7 @@ app.get('/api/products', (req, res) => {
 app.delete('/api/products/:id', (req, res) => {
     try{
         products = products.filter((product) => product.id != req.params.id);
-        res.status(200).json({ products });
+        res.status(200).json({ message: "Product successfully deleted!" });
     } catch (error) {
         res.status(500).json({ error: "Internal server error"});
     }
