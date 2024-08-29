a7cb9ea67741741016aac231d70a37ba14dde75d
Borna <Borna1103@msn.com>
2024-08-29 00:44:11 -0700

    Finished Product

--- StarterCode/backend/index.js
+++ StarterCode/backend/index.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "DELETE"]
+}));
 
 //implement the CORS config
 
@@ -18,19 +24,34 @@ let products = [
     { id: 6, name: 'Product 6', description: 'description 6', price: 50, imageUrl: '' },
 ];
 
+
 //function to generate a url for getting a random image from picsum
 const fetchImageUrl = () => {
     return `https://picsum.photos/200/200?random=${Math.floor(Math.random() * 1000)}`;
 };
 
+products.forEach(product => {
+    product.imageUrl = fetchImageUrl();
+});
+
 //implement the get api for getting products
 app.get('/api/products', (req, res) => {
-
+    try{
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error"});
+    }
+    
 });
 
 //implement the delete api for deleting a product by Id
 app.delete('/api/products/:id', (req, res) => {
-    
+    try{
+        products = products.filter((product) => product.id != req.params.id);
+        res.status(200).json({ products });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error"});
+    }
 });
 
 app.listen(PORT, () => {
--- StarterCode/frontend/src/App/Page/ProductList.jsx
+++ StarterCode/frontend/src/App/Page/ProductList.jsx
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, IconButton , Card, CardMedia, CardContent, Typography} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import axios from 'axios';
+
 
 const ProductList = () => {
+  axios.defaults.baseURL = 'http://localhost:5000/api'
 
-  //implement the get products function
-  const fetchProducts = () => {
+  const [products, setProducts] = useState([]); // State to store fetched products
+
+  /*
+   * Fetches all products
+   */
+  const fetchProducts = async () => {
+    try{
+      const products = await axios({
+        method: 'GET',
+        url: '/products'
+      });
+
+      setProducts(products.data)
+    } catch (error) {
+      console.error('Error fetching products: ', error);
+      return [];
+    }
   };
+  
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-  //implement the delete function
-  const handleDelete = (id) => {
+  /*
+   * Handles deletion of single product
+   */
+  const handleDelete = async (id) => {
+    try{
+      const productId = id.toString();
+      const product = await axios({
+        method: 'DELETE',
+        url: `products/${productId}`,
+      });
+
+      setProducts(products.filter((product) => product.id !== id));   // Remove product on frontend
+    } catch (error) {
+      console.error('Error deleting products: ', error);
+      return [];
+    }
   };
-
+  
+  
   return (
-    <Container >
+    <Container sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', display: 'grid', rowGap: 3, columnGap: 0, gridTemplateColumns: 'repeat(3, 1fr)' }}>
+      {products.map((product) => (
+        <Card sx={{ maxWidth: 345 }}>
+          {/* Delete Button */}
+          <IconButton sx={{ position: 'absolute' }} aria-label="delete" color='error' onClick={() => {
+            handleDelete(product.id);
+            }}>
+              <DeleteIcon />
+          </IconButton>
 
+          {/* Card Image */}
+          <CardMedia
+            sx={{ height: 200 }}
+            image= {product.imageUrl}
+            title="Product Img"
+          />
+          
+          {/* Card Information */}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.name}
+            </Typography>
+            <Typography>
+              ${product.price}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              {product.description}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
     </Container>
   );
 };
